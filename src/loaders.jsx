c65aabdd0c278a1e747fc45fbda97e801028b07f
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-02 22:00:24 -0400

    frontend started, router page, loaders page, index page, show page, app, .env

---
+++ src/loaders.jsx
@@ -0,0 +1,13 @@
+const URL = import.meta.env.VITE_BASE_URL
+
+export const indexLoader = async () => {
+    const response = await fetch(`${URL}`)
+    const allLists = await response.json()
+    return allLists
+}
+
+export const showLoader = async ({ params }) => {
+    const response = await fetch(`${URL}/${params.id}`)
+    const aList = await response.json()
+    return aList
+}
---
+++ src/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import App from "./App"
+import Index from "./pages/Index"
+import Show from "./pages/Show"
+import { indexLoader, showLoader } from "./loaders";
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <>
+        <Route path="/" element={<App/>}>
+            <Route path='' element={<Index/>} loader={indexLoader}/>
+            <Route path='pets/:id' element={<Show/>} loader={showLoader}/>
+            <Route/>
+            <Route/>
+            <Route/>
+        </Route>
+    </>
+))
+
+export default router
