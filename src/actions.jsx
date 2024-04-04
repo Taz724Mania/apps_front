5e28fcc70d5b2d6047218a5255b6551f5f0db825
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-04-03 20:01:20 -0400

    added placeholders to the create fields

--- src/actions.jsx
+++ src/actions.jsx
@@ -2,29 +2,33 @@ import { redirect } from "react-router-dom";
 
 const URL = import.meta.env.VITE_BASE_URL
 
-export const createAction = async ({request}) => {
-    
-    const formData = await request.formData()
+export const createAction = async ({ request }) => {
 
-    
+    const placeholders = {
+      title: "TITLE",
+      date: "DATE",
+      time: "TIME",
+    };
+  
+    const formData = await request.formData();
+  
     const newList = {
-        title: formData.get("title"),
-        date: formData.get("date"),
-        time: formData.get("time"),
-    }
-
-   
+      title: formData.get("title") || placeholders.title,
+      date: formData.get("date") || placeholders.date,
+      time: formData.get("time") || placeholders.time,
+    };
+  
     await fetch(`${URL}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newList)
-    })
-
-    
-    return redirect("/")
-}
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newList),
+    });
+  
+    return redirect("/");
+  };
+  
 
 export const deleteAction = async ({params}) => {
     
